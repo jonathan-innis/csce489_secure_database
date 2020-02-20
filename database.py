29698aa277d4cf1912534662d3a204f134c707f1
Jonathan Innis <jonathan.innis.ji@gmail.com>
2020-02-20 13:46:08 -0600

    Updated the database store

--- database.py
+++ database.py
@@ -1,6 +1,8 @@
+from store import Store
+
 class Database:
     def __init__(self):
         self.principals = {}
-        self.default_delegator = Principal
-        self.local_store = Store
-        self.global_store = Store
+        self.default_delegator = None
+        self.local_store = Store()
+        self.global_store = Store()

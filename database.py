a6e7cc55412ace1cb1ca5fb356deb2e5cc7d9653
Jonathan Innis <jonathan.innis.ji@gmail.com>
2020-02-20 13:59:16 -0600

    Added additional commands for the principal

--- database.py
+++ database.py
@@ -6,3 +6,7 @@ class Database:
         self.default_delegator = None
         self.local_store = Store()
         self.global_store = Store()
+    
+    def create_principal(self, username, password):
+        p = Principal(username, password, self.default_delegator)
+        self.principals[username] = p
--- principal.py
+++ principal.py
@@ -1,16 +1,24 @@
 import hashlib
 
 class Principal:
-    def __init__(self, username, password):
+    def __init__(self, username, password, default_delegator = None):
         self.username = username
         self.password = hashlib.sha256(password)
-        self.local_permissions = {}
-        self.global_permissions = {}
-    
+
+        if default_delegator is None:
+            self.local_permissions = {}
+            self.global_permissions = {}
+        else:
+            self.local_permissions = dict(default_delegator.local_permissions)
+            self.global_permissions = dict(default_delegator.global_permissions)
+
     def authenticate(self, password):
         if hashlib.sha256(password) == self.password:
             return True
         return False
+
+    def change_password(self, new_password):
+        self.password = hashlib.sha256(new_password)
 
     def add_local_permission():
         pass

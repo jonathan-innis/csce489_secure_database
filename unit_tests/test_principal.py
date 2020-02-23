7fadbf3ddd5b8f50f6eefd7a5278e9da77534193
Jonathan Innis <jonathan.innis.ji@gmail.com>
2020-02-23 15:58:45 -0600

    Grouped authentication tests into class

--- unit_tests/test_principal.py
+++ unit_tests/test_principal.py
@@ -1,21 +1,20 @@
 from build.principal import Principal
 
 
-def test_valid_password():
-    p = Principal("test", "password")
-    assert p.authenticate("password")
+class Test_Principal_Authentication:
 
+    def test_valid_password(self):
+        p = Principal("test", "password")
+        assert p.authenticate("password")
 
-def test_invalid_password():
-    p = Principal("test", "password")
-    assert not p.authenticate("pas$word")
+    def test_invalid_password(self):
+        p = Principal("test", "password")
+        assert not p.authenticate("pas$word")
 
+    def test_prefix_password(self):
+        p = Principal("test", "password")
+        assert not p.authenticate("password123")
 
-def test_prefix_password():
-    p = Principal("test", "password")
-    assert not p.authenticate("password123")
-
-
-def test_postfix_password():
-    p = Principal("test", "password")
-    assert not p.authenticate("thisismypassword")
+    def test_postfix_password(self):
+        p = Principal("test", "password")
+        assert not p.authenticate("thisismypassword")

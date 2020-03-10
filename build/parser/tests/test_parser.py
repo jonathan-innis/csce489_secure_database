9b9c2c91e5545b1da5a4e1757065b4c27226165b
Jonathan Innis <jonathan.innis.ji@gmail.com>
2020-03-10 14:09:33 -0500

    Added more test cases for returning and exiting

--- build/parser/tests/test_parser.py
+++ build/parser/tests/test_parser.py
@@ -62,14 +62,158 @@ class Test_Basic_Parse:
             assert ret[-1]["output"] == test["output"]
 
 
-class Test_Append_Parse:
-
-    def test_append_list(self):
-        text1 = 'as principal admin password "admin" do\nset x=[]\nset y =[]\nset z={first= "elem", second= "elem"}\nappend to y with z\n append to y with z\nappend to x with y\nreturn x\n***'
+class Test_Exit_Parse:
+    
+    def test_exit_properly(self):
+        text1 = 'as principal admin password "admin" do\nexit\n***'
 
         test = {
                 "text": text1,
-                "exp_status": ["SET", "SET", "SET", "APPEND", "APPEND", "APPEND", "RETURNING"],
+                "exp_status": ["EXITING"]
+        }
+
+        d = Database("admin")
+        ret = parse(d, test["text"])
+        assert len(ret) == len(test["exp_status"])
+        for i, code in enumerate(ret):
+            assert code["status"] == test["exp_status"][i]
+
+    def test_exit_fails(self):
+        text1 = 'as principal admin password "admin" do\ncreate principal bob "password"\nexit\n***'
+        text2 = 'as principal bob password "password" do\nexit\n***'
+
+        tests = [
+            {
+                "text": text1,
+                "exp_status": ["CREATE_PRINCIPAL", "EXITING"]
+            },
+            {
+                "text": text2,
+                "exp_status": ["DENIED"]
+            }
+        ]
+
+        d = Database("admin")
+        
+        for test in tests:
+            ret = parse(d, test["text"])
+            assert len(ret) == len(test["exp_status"])
+            for i, code in enumerate(ret):
+                assert code["status"] == test["exp_status"][i]
+
+
+class Test_Return_Parse:
+    
+    def test_return_elem(self):
+        text1 = 'as principal admin password "admin" do\nset x = "elem"\nreturn x\n***'
+
+        tests = [
+            {
+                "text": text1,
+                "exp_status": ["SET", "RETURNING"],
+                "output": "elem"
+            },
+        ]
+
+        d = Database("admin")
+        
+        for test in tests:
+            ret = parse(d, test["text"])
+            assert len(ret) == len(test["exp_status"])
+            for i, code in enumerate(ret):
+                assert code["status"] == test["exp_status"][i]
+            assert ret[-1]["output"] == test["output"]
+
+    def test_return_record(self):
+        text1 = 'as principal admin password "admin" do\n set x = {field1="element", field2="element"}\nreturn x\n***'
+
+        tests = [
+            {
+                "text": text1,
+                "exp_status": ["SET", "RETURNING"],
+                "output": {"field1": "element", "field2": "element"}
+            },
+        ]
+
+        d = Database("admin")
+        
+        for test in tests:
+            ret = parse(d, test["text"])
+            assert len(ret) == len(test["exp_status"])
+            for i, code in enumerate(ret):
+                assert code["status"] == test["exp_status"][i]
+            for k in ret[-1]["output"]:
+                assert k in test["output"]
+                assert ret[-1]["output"][k] == test["output"][k] 
+
+    def test_return_record_field(self):
+        text1 = 'as principal admin password "admin" do\n set x ={first="jonathan", last="innis"}\nreturn x.last\n***'
+
+        tests = [
+            {
+                "text": text1,
+                "exp_status": ["SET", "RETURNING"],
+                "output": "innis"
+            },
+        ]
+
+        d = Database("admin")
+        
+        for test in tests:
+            ret = parse(d, test["text"])
+            assert len(ret) == len(test["exp_status"])
+            for i, code in enumerate(ret):
+                assert code["status"] == test["exp_status"][i]
+            assert ret[-1]["output"] == test["output"]
+
+
+class Test_Append_Parse:
+
+    def test_append_list(self):
+        text1 = 'as principal admin password "admin" do\nset x=[]\nset y =[]\nset z={first= "elem", second= "elem"}\nappend to x with "str"\n append to y with z\n append to y with z\nappend to x with y\nreturn x\n***'
+
+        test = {
+                "text": text1,
+                "exp_status": ["SET", "SET", "SET", "APPEND", "APPEND", "APPEND", "APPEND", "RETURNING"],
+                "output": ["str", {"first": "elem", "second": "elem"}, {"first": "elem", "second": "elem"}]
+        }
+
+        d = Database("admin")
+        ret = parse(d, test["text"])
+        assert len(ret) == len(test["exp_status"])
+        for i, code in enumerate(ret):
+            assert code["status"] == test["exp_status"][i]
+        for first, second in zip(test["output"], ret[-1]["output"]):
+            if isinstance(first, dict):
+                for k in first:
+                    assert k in second
+                    assert first[k] == second[k]
+            else:
+                assert first == second
+
+    def test_append_str(self):
+        text1 = 'as principal admin password "admin" do\nset x=[]\nset z="str"\nappend to x with z\n append to x with z\nreturn x\n***'
+
+        test = {
+                "text": text1,
+                "exp_status": ["SET", "SET", "APPEND", "APPEND", "RETURNING"],
+                "output": ["str", "str"]
+        }
+
+        d = Database("admin")
+        ret = parse(d, test["text"])
+        assert len(ret) == len(test["exp_status"])
+        for i, code in enumerate(ret):
+            assert code["status"] == test["exp_status"][i]
+        for first, second in zip(test["output"], ret[-1]["output"]):
+            assert first == second
+
+    def test_append_dict(self):
+        text1 = 'as principal admin password "admin" do\nset x=[]\nset z={first="elem", second="elem"}\nappend to x with z\n append to x with z\nreturn x\n***'
+
+        test = {
+                "text": text1,
+                "exp_status": ["SET", "SET", "APPEND", "APPEND", "RETURNING"],
                 "output": [{"first": "elem", "second": "elem"}, {"first": "elem", "second": "elem"}]
         }
 

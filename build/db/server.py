310620e0bbb9f804e727d65fb854a7c7eb6a6228
patrickmpalmer <patrickpalmer271@gmail.com>
2020-03-15 00:57:16 -0500

    Starting server now initiates a db, and the handler calls the parser

--- build/db/server.py
+++ build/db/server.py
@@ -1,10 +1,18 @@
+from parser.parser import Parser
 import socketserver
 
 
 class TCPHandler(socketserver.BaseRequestHandler):
+    def __init__(self, database, *args, **kwargs):
+        self.__database = database
+        super().__init__(*args, **kwargs)
+
     def handle(self):
         # self.request is the TCP socket connected to the client
-        self.data = self.request.recv(1024).strip()
-        print("{} wrote:".format(self.client_address[0]))
-        print(self.data)
+        self.data = self.request.recv(1000000).strip()  # limit of programs is 1 million chars - is this the correct way to measure that?
+        parser = Parser()
+        reply = parser.parse(self.__database, self.data.decode())
+        # print("{} wrote:".format(self.client_address[0]))
+        print(self.data.decode())
+        print(reply)
         self.request.sendall(b"Message recieved: " + self.data + b"\n")
